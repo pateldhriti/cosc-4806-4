dea3b025835dec0fe3f726a1e2c8574ccd23e1a5
pateldhriti <135610241+pateldhriti@users.noreply.github.com>
2025-07-04 19:20:33 +0000

    make changes in all fixing error

--- app/models/Reminder.php
+++ app/models/Reminder.php
@@ -12,6 +12,17 @@ class Reminder {
         return $rows;
     }
 
+    public function create_reminder($user_id, $subject) {
+        $db = db_connect();
+        $stmt = $db->prepare("INSERT INTO notes (user_id, subject, created_at, completed, deleted) 
+                              VALUES (:user_id, :subject, NOW(), 0, 0)");
+        return $stmt->execute([
+            'user_id' => $user_id,
+            'subject' => $subject
+        ]);
+    }
+
+
     public function update_reminder ($reminder_id, $new_subject) {
         $db = db_connect();
         $stmt = $db->prepare("update notes set subject = :subject where id = :id");
@@ -19,7 +30,15 @@ class Reminder {
         'subject' => $new_subject,
         'id' => $reminder_id
          ]);    
-        
+
     }
+
+    public function get_reminder_by_id($id) {
+        $db = db_connect();
+        $stmt = $db->prepare("SELECT * FROM notes WHERE id = :id");
+        $stmt->execute(['id' => $id]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
 }
 ?>
