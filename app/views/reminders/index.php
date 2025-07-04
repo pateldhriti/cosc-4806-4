bf642a1c9d63fa1ab40ed5bdd96dfd1eb8162008
pateldhriti <135610241+pateldhriti@users.noreply.github.com>
2025-07-04 18:29:18 +0000

    Add create, edit, and delete buttons with Bootstrap styling in reminders view

--- app/views/reminders/index.php
+++ app/views/reminders/index.php
@@ -2,18 +2,29 @@
 <div class="container">
     <div class="page-header" id="banner">  
         <div class="row">
-           <div class="col-lg-12">
+            <div class="col-lg-12">
                 <h1>Reminders</h1>
-           </div>
+                <a href="/reminders/create" class="btn btn-primary mb-3">+ Add Reminder</a>
+            </div>
         </div>
     </div>
 
-    <ul>
-        <?php foreach ($data['reminders'] as $note): ?>
-            <li><?= htmlspecialchars($note['subject']) ?></li>
-        <?php endforeach; ?>
-    </ul>
+    <div class="card">
+        <div class="card-body">
+            <ul class="list-group">
+                <?php foreach ($data['reminders'] as $note): ?>
+                    <li class="list-group-item d-flex justify-content-between align-items-center">
+                        <?= htmlspecialchars($note['subject']) ?>
+                        <div>
+                            <a href="/reminders/edit/<?= $note['id'] ?>" class="btn btn-sm btn-warning me-1">Edit</a>
+                            <a href="/reminders/delete/<?= $note['id'] ?>" class="btn btn-sm btn-danger"
+                               onclick="return confirm('Are you sure you want to delete this reminder?');">Delete</a>
+                        </div>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+        </div>
+    </div>
+</div>
 
-    <?php require_once 'app/views/templates/footer.php'; 
-
-?>
+<?php require_once 'app/views/templates/footer.php'; ?>
