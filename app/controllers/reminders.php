ecca3baa4377e56c82b7b67049a58eb61945076f
pateldhriti <135610241+pateldhriti@users.noreply.github.com>
2025-07-04 17:45:17 +0000

    Add debug code to print list of reminders in Reminders controller

--- app/controllers/reminders.php
+++ app/controllers/reminders.php
@@ -4,7 +4,9 @@ class Reminders extends Controller {
   
     public function index() {
       $reminder = $this->model('Reminder');
-      $reminders = $reminder->get_all_reminders();
+      $list_of_reminders = $reminder->get_all_reminders();
+   print_r($list_of_reminders);
+      die;
         $this->view('reminders/index');
     }
 
